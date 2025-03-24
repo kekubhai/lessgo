ad7e3f655cd883d9a5c3a6cd14a5f7d443e9b593
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-03-24 08:40:56 +0530

    Refactor LinkedIn scraper implementation; move to separate file and update import statements

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from datetime import datetime
 from scrapers import LinkedInScraper, TelegramScraper  # TwitterScraper commented out
 from notifiers import WhatsAppNotifier
 import config
-
+from .linkedin import LinkedInScraper
+from .telegram import TelegramScraper
 def scrape_and_notify():
     """Main function that handles job scraping and notifications"""
     
